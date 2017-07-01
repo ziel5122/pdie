2b6c4591f9a1744a28437891cd1edf44f8645242
Austin Zielinski <azielinski@csumb.edu>
2017-06-30 18:05:08 -0700

    touched up header

--- src/Header/header.js
+++ src/Header/header.js
@@ -5,11 +5,16 @@ import styles from './styles';
 const Header = () => (
   <div style={styles.header}>
     {`some header text`}
-    <img
-      alt="panafold"
-      src="https://firebasestorage.googleapis.com/v0/b/pdie-image-storage.appspot.com/o/img%2Fpanafold.png?alt=media&token=1a7043c7-c4b3-498f-852c-71ee37341da4"
-      style={styles.panafold}
-    />
+    <a href="http://panafold.co">
+      <img
+        alt="panafold"
+        onClick={() => {
+          window.location.href = 'https://panafold.co/';
+        }}
+        src="https://firebasestorage.googleapis.com/v0/b/pdie-image-storage.appspot.com/o/img%2Fpanafold.png?alt=media&token=1a7043c7-c4b3-498f-852c-71ee37341da4"
+        style={styles.panafold}
+      />  
+    </a>
   </div>
 );
 
