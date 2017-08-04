6fa3c180264a040bc0027a67e8323bd234cd74d5
ziel5122 <azielinski@csumb.edu>
2017-08-04 15:16:49 -0700

    added webpack bundle analyzer

--- webpack.config.client.babel.js
+++ webpack.config.client.babel.js
@@ -1,5 +1,6 @@
 import { join } from 'path';
-
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import webpack from 'webpack';
 const APP_DIR = join(__dirname, 'src');
 
 const config = {
@@ -30,6 +31,19 @@ const config = {
     path: join(__dirname, 'build'),
     filename: '[name].bundle.js',
   },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      filename: 'node-static.js',
+      minChunks({ context }, count) {
+        return context && context.indexOf('node_modules') >= 0;
+      },
+      name: 'node-static',
+    }),
+  ],
 };
 
 export default config;
