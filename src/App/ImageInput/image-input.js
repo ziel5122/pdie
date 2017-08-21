c064a58c91b8380d13cdd8043b4b9c5aa4921d5c
ziel5122 <azielinski@csumb.edu>
2017-08-21 00:18:47 -0700

    working on vision API

--- src/App/ImageInput/image-input.js
+++ src/App/ImageInput/image-input.js
@@ -1,17 +1,44 @@
 import React from 'react';
 
-import { gcv } from '../../backend/gcp';
+import config from '../../../config/gcp';
+
+const labelUrl = `https://vision.googleapis.com/v1/images:annotate?key=${config.apiKey}`;
 
 let reader;
 
 const ImageInput = ({ setImageUploadUrl }) => {
-  console.log('this updated');
-
   if (typeof window !== 'undefined') {
     reader = new FileReader();
     reader.onload = () => {
       const { result } = reader;
       setImageUploadUrl(result);
+
+      const request = {
+        requests: [
+          {
+            image: {
+              source: {
+                imageUri: result,
+              },
+            },
+            features: [
+              {
+                type: 'LABEL_DETECTION',
+              },
+            ],
+          },
+        ],
+      };
+
+      fetch(labelUrl, {
+        method: 'post',
+        headers: new Headers({
+          'content-type': 'application/json',
+        }),
+        body: request,
+      }).then((response) => {
+        return response.json();
+      }).then(responseJson => console.log(responseJson.labelAnnotations));
     }
   }
 
--- src/index.js
+++ src/index.js
@@ -8,15 +8,6 @@ import reducers from './reducers';
 const store = createStore(reducers, window.__PRELOADED_STATE);
 delete window.__PRELOADED_STATE;
 
-const vision = () => {
-  const { imageUploadUrl } = store.getState().image;
-
-  console.log('triggered');
-  console.log(imageUploadUrl.substr(0, 50));  
-};
-
-store.subscribe(vision);
-
 render(
   <Main store={store} />,
   document.getElementById('root')
